9fa6020bf97e8a43e9e663a5f84ed4c6432aa4a9
phu-phurithat <phurithat.komkham@gmail.com>
2025-02-03 14:26:01 +0700

    setup docker

--- employee-service/src/test/java/com/phu/service/EmployeeserviceApplicationTests.java
+++ employee-service/src/test/java/com/phu/service/EmployeeserviceApplicationTests.java
@@ -1,11 +1,13 @@
-package com.phu.employee;
+package com.phu.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.test.context.ContextConfiguration;
 
 @EnableDiscoveryClient
-@SpringBootTest
+@SpringBootTest(classes = EmployeeserviceApplication.class)
+@ContextConfiguration(classes = EmployeeserviceApplication.class)
 class EmployeeserviceApplicationTests {
 
 	@Test
